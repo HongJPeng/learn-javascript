386de102ee258c721a05d79ace5dc22fef153bb9
HPeng <hongjpeng@gmail.com>
2022-04-02 20:15:10 -0400

    completed the homework

--- 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
+++ 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
@@ -1,3 +1,21 @@
+/*  -- Button-- 
+initialize the count as 0
+listen for clicks on the increment button
+increment the count variable when the button is clicked
+change the count-el in the HTML to reflect the new count
+*/
+let count = 0
+function increment(){
+    console.log("The button was clicked")
+}
+
+count = count + increment
+
+
+
+
+
+/*
 let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
@@ -15,3 +33,27 @@ function save() {
 }
 
 console.log("Let's count people on the subway!")
+*/
+/* --My Dog Age--
+let myAge = 56
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+*/
+
+/*  --bounsPoints Count--
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bounsPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bounsPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bounsPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+*/
